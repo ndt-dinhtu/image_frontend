d70cd0ac2adeea91ba506b62283bf5434e50a031
ndt-dinhtu <ndt.290303060703@gmail.com>
2025-01-10 14:32:58 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Admin from "./components/Admin/Admin";
 import User from "./components/User/User";
 
-const Appp = () => {
+const App = () => {
   return (
-    <div>
-      <Admin />
-      <User />
-    </div>
+    <BrowserRouter future={{ v7_startTransition: true }}>
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/admin/*" element={<Admin />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
-export default Appp;
+export default App;
